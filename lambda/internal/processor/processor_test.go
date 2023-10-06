3a44f07100b16bf2d7dfd34754709d0f7c10031e
AndrewCharlesHay <39sumer3939@gmail.com>
2023-10-06 09:09:17 -0500

    idk

--- lambda/internal/processor/processor_test.go
+++ lambda/internal/processor/processor_test.go
@@ -21,39 +21,6 @@ type mockDatabase struct {
 	mock.Mock
 }
 
-func (m *mockDatabase) StoreCheckpoint(record *entity.CheckpointRecord) error {
-	args := m.Called(record)
-	return args.Error(0)
-}
-
-func (m *mockDatabase) GetCheckpoint(id string) (*entity.CheckpointRecord, error) {
-	args := m.Called(id)
-	return args.Get(0).(*entity.CheckpointRecord), args.Error(1)
-}
-
-type mockLogCollector struct {
-	logcollector.LogCollector
-	mock.Mock
-}
-
-func (m *mockLogCollector) GetLogs(timestamp int64) (io.Reader, bool, int64, error) {
-	args := m.Called(timestamp)
-	if args.Get(0) == nil {
-		return nil, args.Get(1).(bool), args.Get(2).(int64), args.Error(3)
-	}
-	return args.Get(0).(io.Reader), args.Get(1).(bool), args.Get(2).(int64), args.Error(3)
-}
-
-type mockWriter struct {
-	s3writer.Writer
-	mock.Mock
-}
-
-func (m *mockWriter) WriteLogEntry(data entity.LogEntry) error {
-	args := m.Called(data)
-	return args.Error(0)
-}
-
 func TestProcessMultiLogCallback(t *testing.T) {
 	p := parser.NewAuditLogParser()
 	lc := new(mockLogCollector)
